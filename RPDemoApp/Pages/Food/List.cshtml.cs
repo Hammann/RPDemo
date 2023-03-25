7a8511a96e1edbbf8355b7d6ffbfabf026b3e9fd
Hammann <brandenhammann@gmail.com>
2023-03-25 18:32:39 -0500

    Add project files.

---
+++ RPDemoApp/Pages/Food/List.cshtml.cs
@@ -0,0 +1,25 @@
+using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RPDemoApp.Pages.Food
+{
+    public class ListModel : PageModel
+    {
+        private readonly IFoodData _foodData;
+
+        public List<FoodModel> Food { get; set; }
+
+        public ListModel(IFoodData foodData)
+        {
+            _foodData = foodData;
+        }
+        public async Task OnGet()
+        {
+            Food = await _foodData.GetFood();
+        }
+    }
+}
